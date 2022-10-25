fbedc54c6667c39b014bc5ea3b9734537a5c6cc8
withoutspaces <dms@ic.ufal.br>
2022-10-25 09:51:16 -0300

    connect routes with databse

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,9 +1,12 @@
 module.exports = app => {
   const Tasks = app.models.tasks
 
-  app.get('/tasks', (req, res) => {
-    Tasks.findAll({}, tasks => {
-      res.json(tasks)
-    })
+  app.get('/tasks', async (req, res) => {
+    try {
+      const tasks = await Tasks.findAll()
+      res.json({ tasks })
+    } catch (error) {
+      res.status(500).json(error)
+    }
   })
 }
