dd3a36b1da428c82523af983ca48ac77a83bfd52
withoutspaces <dms@ic.ufal.br>
2022-10-26 21:15:12 -0300

    feat: tasks route

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,12 +1,59 @@
 module.exports = app => {
   const Tasks = app.models.tasks
 
-  app.get('/tasks', async (req, res) => {
-    try {
-      const tasks = await Tasks.findAll()
-      res.json({ tasks })
-    } catch (error) {
-      res.status(500).json(error)
-    }
-  })
+  app
+    .route('/tasks')
+    .get(async (req, res) => {
+      try {
+        const results = await Tasks.findAll()
+        res.json(results)
+      } catch (error) {
+        res.status(412).json({ msg: error.message })
+      }
+    })
+    .post(async (req, res) => {
+      try {
+        const result = await Tasks.create(req.body)
+        res.json(result)
+      } catch (error) {
+        res.status(412).json({ msg: error.message })
+      }
+    })
+
+  app
+    .route('/tasks/:id')
+    .get(async (req, res) => {
+      try {
+        const { id } = req.params
+        const where = { id } // {'id': id}
+        const result = await Tasks.findOne({ where }) // {'where': {'id': id}}
+        if (result) {
+          res.json(result)
+        } else {
+          res.sendStatus(404)
+        }
+      } catch (error) {
+        res.status(412).json({ msg: error.message })
+      }
+    })
+    .put(async (req, res) => {
+      try {
+        const { id } = req.params
+        const where = { id }
+        await Tasks.update(req.body, { where })
+        res.sendStatus(204)
+      } catch (error) {
+        res.status(412).json({ msg: error.message })
+      }
+    })
+    .delete(async (req, res) => {
+      try {
+        const { id } = req.params
+        const where = { id }
+        await Tasks.destroy({ where })
+        res.sendStatus(204)
+      } catch (error) {
+        res.status(412).json({ msg: error.message })
+      }
+    })
 }
