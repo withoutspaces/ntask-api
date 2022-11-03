91827a0a82d0369a5d99dc9268503a03e65481d1
withoutspaces <dms@ic.ufal.br>
2022-11-03 19:49:58 -0300

    db config to tests

--- src/libs/boot.js
+++ src/libs/boot.js
@@ -3,9 +3,11 @@ module.exports = app => {
     try {
       await app.db.authenticate()
       await app.db.sync()
-      app.listen(port, () => {
-        console.log(`NTASK API - porta ${port}`)
-      })
+      if (process.env.NODE_ENV !== 'test') {
+        app.listen(port, () => {
+          console.log(`NTASK API - porta ${port}`)
+        })
+      }
     } catch (error) {
       console.log('Erro de conexão com o banco de dados.')
       console.log(err)
